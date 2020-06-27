3fb5fdd05d2b39bc20aaaf08d73f3a9c38bd681e
solitaryclown <57932378+solitaryclown@users.noreply.github.com>
2020-06-27 22:42:33 +0800

    增加音效播放类PlayMusic

---
+++ PlayMusic.java
@@ -0,0 +1,49 @@
+
+
+
+
+import java.applet.AudioClip;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import sun.audio.*;
+public class PlayMusic{
+	 File file;
+	 AudioInputStream audioInputStream;
+	 public void playSound(String filename) {
+		    try {
+		    	
+		        audioInputStream= AudioSystem.getAudioInputStream(new File(filename).getAbsoluteFile());
+		        Clip clip = AudioSystem.getClip();
+		        clip.open(audioInputStream);
+		        clip.start();
+		        
+		        Thread.sleep(1);
+		    } catch(Exception ex) {
+		        System.out.println("Error with playing sound.");
+		        ex.printStackTrace();
+		    }
+	
+	}
+	 //±≥æ∞“Ù¿÷
+	 public void playLoop(String filename) {
+		    try {
+		    	
+		        audioInputStream= AudioSystem.getAudioInputStream(new File(filename).getAbsoluteFile());
+		        Clip clip = AudioSystem.getClip();
+		        clip.open(audioInputStream);
+		        clip.loop(Clip.LOOP_CONTINUOUSLY);//—≠ª∑≤•∑≈
+		        
+		    } catch(Exception ex) {
+		        System.out.println("Error with playing sound.");
+		        ex.printStackTrace();
+		    }
+	
+	}
+	
+}
